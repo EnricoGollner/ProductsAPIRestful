095ab39d618d713c3ba6aea8889443a43f631598
Enrico Gollner <enricogollner.tech@gmail.com>
2023-07-26 06:09:53 -0300

    get e post controllers

--- productsAPIRestful/src/main/java/dev/enricogollner/productsAPIRestful/ProductController/ProductController.java
+++ productsAPIRestful/src/main/java/dev/enricogollner/productsAPIRestful/ProductController/ProductController.java
@@ -11,6 +11,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 public class ProductController {
@@ -18,17 +21,40 @@ public class ProductController {
     ProductRepository productRepository;
 
     @GetMapping("/products")
-    public ResponseEntity<List<ProductModel>> getProducts() {
-        return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.findAll());
+    public ResponseEntity<List<ProductModel>> getAllProducts() {
+        return ResponseEntity.status(HttpStatus.OK).body(productRepository.findAll());
     }
+    @GetMapping("/products/{id}")
+    public ResponseEntity<Object> getOneProduct(@PathVariable(value = "id") UUID idProduct) {
+        Optional<ProductModel> product0 = productRepository.findById(idProduct);
 
+        if (product0.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found.");
+        }
 
+        return ResponseEntity.status(HttpStatus.OK).body(product0.get());
+    }
 
     @PostMapping("/products")
     public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductRecordDTO productRecordDto) {
         var productModel = new ProductModel();
         BeanUtils.copyProperties(productRecordDto, productModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));  // HttpStatus.CREATED é o 200
+    }
+
+    @PutMapping("/products/{id}")
+    public ResponseEntity<Object> updateProject(@PathVariable(value = "id") UUID productId,
+                                                @RequestBody @Valid ProductRecordDTO productRecordDTO) {
+        Optional<ProductModel> product0 = productRepository.findById(productId);
+
+        if (product0.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Product not found");
+        }
+
+        var productModel = product0.get();  // auxiliar para atualizar sem gerar um novo id
+        BeanUtils.copyProperties(productRecordDTO, productModel);  // atualizando SOMENTE campos que foram alterados do dto para o model
+        
+        return ResponseEntity.status(HttpStatus.OK).body(productRepository.save(productModel));
     }
 
 }
