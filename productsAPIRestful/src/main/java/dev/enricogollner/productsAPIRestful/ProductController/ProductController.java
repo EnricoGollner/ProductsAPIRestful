5f3f640694af818af311bc5e6c2768c6ec915c86
Enrico Gollner <enricogollner.tech@gmail.com>
2023-07-26 18:06:37 -0300

    V1.0

--- productsAPIRestful/src/main/java/dev/enricogollner/productsAPIRestful/ProductController/ProductController.java
+++ productsAPIRestful/src/main/java/dev/enricogollner/productsAPIRestful/ProductController/ProductController.java
@@ -11,9 +11,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.UUID;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 
 @RestController
 public class ProductController {
@@ -22,7 +25,16 @@ public class ProductController {
 
     @GetMapping("/products")
     public ResponseEntity<List<ProductModel>> getAllProducts() {
-        return ResponseEntity.status(HttpStatus.OK).body(productRepository.findAll());
+        List<ProductModel> productsList = productRepository.findAll();
+
+        if (!productsList.isEmpty()) {
+            for(ProductModel product : productsList) {
+                UUID productId = product.getId();
+                product.add(linkTo(methodOn(ProductController.class).getOneProduct(productId)).withSelfRel());
+            }
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(productsList);
     }
     @GetMapping("/products/{id}")
     public ResponseEntity<Object> getOneProduct(@PathVariable(value = "id") UUID idProduct) {
