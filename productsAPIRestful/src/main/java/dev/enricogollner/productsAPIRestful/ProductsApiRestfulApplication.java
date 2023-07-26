066905983ce9f9de00184aad331c42e33d1e99f1
Enrico Gollner <enricogollner.tech@gmail.com>
2023-07-26 04:17:14 -0300

    Preparando script pg-docker e  pgadmin-docker

---
+++ productsAPIRestful/src/main/java/dev/enricogollner/productsAPIRestful/ProductsApiRestfulApplication.java
@@ -0,0 +1,13 @@
+package dev.enricogollner.productsAPIRestful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsApiRestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsApiRestfulApplication.class, args);
+	}
+
+}
---
+++ productsAPIRestful/src/test/java/dev/enricogollner/productsAPIRestful/ProductsApiRestfulApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.enricogollner.productsAPIRestful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsApiRestfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
